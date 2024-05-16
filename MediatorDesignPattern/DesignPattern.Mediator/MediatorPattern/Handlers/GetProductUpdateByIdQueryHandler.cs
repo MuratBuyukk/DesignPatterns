24103e5c836fea9fbca4c20b1e334b467b7f5215
MuratBuyukk <murat_buyuk_1999@hotmail.com>
2024-05-16 23:58:53 +0300

    Mediator Design Pattern..!

---
+++ MediatorDesignPattern/DesignPattern.Mediator/MediatorPattern/Handlers/GetProductUpdateByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using DesignPattern.Mediator.DAL;
+using DesignPattern.Mediator.MediatorPattern.Queries;
+using DesignPattern.Mediator.MediatorPattern.Results;
+using MediatR;
+
+namespace DesignPattern.Mediator.MediatorPattern.Handlers
+{
+    public class GetProductUpdateByIdQueryHandler : IRequestHandler<GetProductUpdateByIdQuery, UpdateProductByIdQueryResult>
+    {
+        private readonly Context _context;
+
+        public GetProductUpdateByIdQueryHandler(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<UpdateProductByIdQueryResult> Handle(GetProductUpdateByIdQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _context.Products.FindAsync(request.Id);
+            return new UpdateProductByIdQueryResult
+            {
+                ProductID = value.ProductID,
+                ProductName = value.ProductName,
+                ProductPrice = value.ProductPrice,
+                ProductStock = value.ProductStock,
+                ProductCategory = value.ProductCategory,
+                ProductStockType = value.ProductStockType,
+            };
+        }
+    }
+}
